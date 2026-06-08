6a0f49b5d434bfc9a3df150e12a82b9e3a314f17
Ny Aina Fandresena <ainafandresena2006@gmail.com.com>
2026-06-08 07:45:25 -0700

    maj login depe

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-// App.js
+// src/App.js
 import React, { useState } from 'react';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
@@ -14,34 +13,8 @@ function App() {
       ) : (
         <SignUp onSwitchToLogin={() => setIsLogin(true)} />
       )}
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> c0f3bdd00fb20766f3c01b480a0aa87e5eac0190
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> c0f3bdd00fb20766f3c01b480a0aa87e5eac0190
